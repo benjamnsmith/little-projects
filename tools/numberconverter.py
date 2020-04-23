da3d3a0035a9209d2c30f2403e473e09d6ef39ad
Ben Smith <smit9524@umn.edu>
2020-04-23 14:06:12 -0500

    number converter tool

---
+++ tools/numberconverter.py
@@ -0,0 +1,57 @@
+import sys
+
+# I have this set up in my bash profile as:
+#   alias convert="python 3 PATH/TO/FILE/numberconverter.py"
+# So that I can call it with convert <type> <number>
+
+
+# arg 1: type of num (dec, hex, bin, oct)
+# arg 2: num
+
+def decimal(num):
+    print("Binary:      ",bin(num))
+    print("Hexadecimal: ",hex(num)) 
+    print("Octal:       ", oct(num))
+    return
+
+def hexa(num):
+    print("Decimal:     ",int(num,16))
+    print("Binary:      ", bin(int(num,16)))
+    print("Octal:       ", oct(int(num,16)))
+
+def bina(num):
+    print("Decimal:     ",int(num,2))
+    print("Hexadecimal: ", hex(int(num,2)))
+    print("Octal:       ",oct(int(num,2)))
+
+def octa(num):
+    print("Decimal:     ",int(num,8))
+    print("Hexadecimal: ",hex(int(num,8)))
+    print("Binary:      ",bin(int(num,8)))
+
+def main():
+    t = sys.argv[1]
+    num = sys.argv[2]
+    if (t == "dec"):
+        try:
+            num = eval(num)
+            decimal(num)
+        except NameError:
+            print("That is not a valid number")
+    elif (t == "hex"):
+        hexa(num)
+    elif (t == "bin"):
+        try:
+            bina(num)
+        except ValueError:
+            print("That is not a valid number")
+    elif (t == "oct"):
+        try:
+            octa(num)
+        except ValueError:
+            print("That is not a valid number")
+    else:
+        print("format not recongnized\n")
+
+if (__name__ == "__main__"):
+    main()
