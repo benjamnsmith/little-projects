585383d3c68555aae8616ace7defef1871a6e2ab
Ben Smith <smit9524@umn.edu>
2022-01-29 18:05:44 -0600

    more functionality, removed need to specify format

--- python/numberconverter.py
+++ python/numberconverter.py
@@ -1,12 +1,11 @@
 import sys
+import re
 
 # I have this set up in my bash profile as:
 #   alias convert="python3 PATH/TO/FILE/numberconverter.py"
-# So that I can call it with convert <type> <number>
+# So that I can call it with convert <number>
 
-
-# arg 1: type of num (dec, hex, bin, oct)
-# arg 2: num
+# arg 1: num
 
 def decimal(num):
     print("Binary:      ", bin(num))
@@ -29,51 +28,51 @@ def octa(num):
     print("Hexadecimal: ", hex(int(num,8)))
     print("Binary:      ", bin(int(num,8)))
 
+def get_format(num):
+    match num[:2]:
+        case "0x": return "hex"
+        case "0o": return "oct"
+        case "0b": return "bin"
+        case _: return "dec"
+
 def main():
-    if len(sys.argv) == 3:
-        t = sys.argv[1]
-        num = sys.argv[2]
+    if len(sys.argv) == 2:
+        num = sys.argv[1]
     elif len(sys.argv) == 2 and sys.argv[1] == '-h':
         print("\nNUMBER CONVERTER HELP")
-        print("Usage: numberconverter.py <FORMAT> <NUMBER>")
-        print("  Where <FORMAT> is the current format of your number, which is one of the following:")
-        print("    - dec")
-        print("    - bin")
-        print("    - hex")
-        print("    - oct")
+        print("Usage: numberconverter.py <NUMBER>")
         print("  Where <NUMBER> is the number you would like to convert. Please use prefix 0x for") 
         print("  hex numbers, 0b for binary, and 0o for octal. Decimal numbers require no prefix.\n")
-        print("If one or both arguments are omitted, the program will ask you to interactively enter")
-        print("your numbers and formats\n")
+        print("If the argument is omitted, the program will ask you to interactively enter")
+        print("your number\n")
         exit()
     else:
-        t = input("Please enter the format of your number (dec, hex, bin, oct): ")
-        num = input("Please enter your numbner: ")
+        num = input("Please enter your number: ")
+    t = get_format(num)
     print("\nYou entered %s. This number is in %s format.\n" % (num, t))
     match t:
         case "dec":
             try:
                 num = eval(num)
                 decimal(num)
-            except NameError:
+            except:
                 print("That is not a valid number")
         case "hex":
                 try: 
                     hexa(num)
-                except ValueError:
+                except:
                     print("That is not a valid number")
         case "bin" :
                 try:
                     bina(num)
-                except ValueError:
+                except:
                     print("That is not a valid number")
         case "oct":
                 try:
                     octa(num)
-                except ValueError:
+                except:
                     print("That is not a valid number")
-        case _:
-            print("format not recongnized\n")
+    exit()
 
 if (__name__ == "__main__"):
     main()
