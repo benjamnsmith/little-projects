e00a3016ebea11db05b66c5cff559ad09259c44e
Ben Smith <smit9524@umn.edu>
2022-02-04 16:34:36 -0600

    updated recommender system

--- python/wordle.py
+++ python/wordle.py
@@ -9,7 +9,6 @@ goal = [-1, -1, -1, -1, -1]
 ans = [' ', ' ', ' ', ' ', ' ']
 confirmed = {}
 confirmed_not = []
-num_recs = 0
 recs = {}
 
 # FIRST PASS
@@ -17,6 +16,7 @@ recs = {}
 # ie. If we know the word has an 'h', remove all words without an 'h'
 def pass_one(confirmed):
     global recs
+    global num_recs
 
     for rec in recs:
         for conf in confirmed:
@@ -26,8 +26,10 @@ def pass_one(confirmed):
 # SECOND PASS
 # Remove words that don't have for-certain letters in the right location
 # ie. if we know the word has an 'e' in position 2, remove words that don't have an 'e' as the second letter
-def pass_two(confirmed):
+def pass_two():
     global recs
+    global goal
+    global num_recs
 
     for rec in recs:
         for i in range(len(goal)):
@@ -49,7 +51,7 @@ def pass_three(confirmed):
                 curlist = confirmed[letter]
                 if rec[curlist[i]] == letter:
                     recs[rec] = -1
-                    
+
 # ZERO PASS
 # Remove words that have letters we know are not in the word
 # ie. if we know the word does not contain a 't', remove all words with 't'
@@ -64,13 +66,25 @@ def getRecommendations(letters):
     global confirmed_not
 
     pass_zero(confirmed_not)
+    
 
     pass_one(letters)
 
-    pass_two(letters)
+
+    pass_two()
+
 
     pass_three(letters)
 
+
+def countRecs():
+    global recs
+    total = 0
+
+    for el in recs:
+        if recs[el] == 1:
+            total += 1
+    return total
 
 def initRecs():
     global recs
@@ -100,52 +114,83 @@ def printKnown(lst):
     for el in lst:
         print("%s" % (el), end = " ")
 
-def printRecs():
+def filterRecs():
     global recs
-    for r in recs:
-        if recs[r] > 0:
-            print(r, end = " ")
-    print("\n")
 
-print("\n=== GET WORDLE RECOMMENDATIONS LIVE ===\n")
-print("  We will give you live wordle recommendations - all you have to do is come up with a starting word!")
+    tmp = recs
+
+    least_common = ['q', 'j', 'z', 'x', 'v', 'k', 'w', 'y', 'f', 'b', 'g', 'h', 'm']
+
+    for rec in tmp:
+        for letter in least_common:
+            if letter in rec:
+                tmp[rec] = -1
+    return tmp
+
+
+def printRecs():
+    if countRecs() > 20:
+        here = filterRecs()
+    else:
+        global recs
+        here = recs
+    message = False
+    iter = 0
+    for r in here:
+        if recs[r] > 0:
+            if not message:
+                print("Here are my recommendations:")
+                message = True
+            print(r, end = " ")
+            iter += 1
+            if iter % 9 == 0:
+                print()
+                iter = 0
+    print()
+
+print("\n========= GET LIVE WORDLE RECOMMENDATIONS =========\n")
+print("  We will give you live wordle recommendations")
+print("  all you have to do is come up with a starting")
+print("  word!")
 
 initRecs()
-
-while not done:
-    guess = input("Please input your guess: ")
-    print("Please tell us how your word performed:")
-    print("0 - not present\n1 - present, wrong location\n2 - present, right location\n")
-    for i in range(5):
-        if (ans[i] == ' '):
-            goal[i] = int(input("%c - " % (guess[i])))
-        else:
-            print("%c - CONFIRMED" % ans[i])
-            continue
-        if goal[i] == 2:
-            ans[i] = guess[i]
-            if (guess[i] not in confirmed):
-                confirmed[guess[i]] = [i]
-        if goal[i] == 1:
-            if (guess[i] not in confirmed):
-                confirmed[guess[i]] = [i]
+try:
+    while not done:
+        print("---------------------------------------------------")
+        guess = input("Please input your guess: ")
+        print("Please tell us how your word performed:")
+        print("0 - not present\n1 - present, wrong location\n2 - present, right location\n")
+        for i in range(5):
+            if (ans[i] == ' '):
+                goal[i] = int(input("%c - " % (guess[i])))
             else:
-                confirmed[guess[i]].append(i)
-            print(confirmed)
-        if goal[i] == 0:
-            if ((guess[i] not in confirmed_not) and (guess[i] not in confirmed)):
-                confirmed_not.append(guess[i])
-                # print(confirmed_not)
-    if checkDone(ans):
-        print("Congrats! You're all done")
-        break
-    print("Alright, here's where we're at:")
-    printWord(ans)
-    for c in confirmed:
-        if c not in ans:
-            print(c, end = " ")
-    print("\n")
-    getRecommendations(confirmed)
-    printRecs()
-
-    
+                print("%c - CONFIRMED" % ans[i])
+                continue
+            if goal[i] == 2:
+                ans[i] = guess[i]
+            if goal[i] == 1:
+                if (guess[i] not in confirmed):
+                    confirmed[guess[i]] = [i]
+                else:
+                    confirmed[guess[i]].append(i)
+                print(confirmed)
+            if goal[i] == 0:
+                if ((guess[i] not in confirmed_not) and (guess[i] not in confirmed)):
+                    confirmed_not.append(guess[i])
+        if checkDone(ans):
+            print("Congrats! You're all done")
+            break
+        print("---------------------------------------------------")
+        print("Alright, here's where we're at:")
+        printWord(ans)
+        print("Here's the other letters we know are in the word somewhere:")
+        for c in confirmed:
+            if c not in ans:
+                print(c, end = " ")
+        print("\n---------------------------------------------------")
+        getRecommendations(confirmed)
+        printRecs()
+except KeyboardInterrupt:
+    print("\n\nThanks for playing! Now shutting down...\n")
+finally:
+    exit(0)
